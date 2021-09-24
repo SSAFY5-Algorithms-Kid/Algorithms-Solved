9a5294a56851cc17cfe21fb8ca29d64e932764c4
심재우 <jw_shim@naver.com>
2021-09-25 00:57:39 +0900

    210925 : BOJ 10869 심재우

---
+++ Baekjoon/10869/Main_심재우.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_BOJ_10869_사칙연산_B5 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
