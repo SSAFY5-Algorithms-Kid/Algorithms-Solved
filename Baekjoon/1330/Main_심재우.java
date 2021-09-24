1f02679f6d2f7659a82edb6f803ca0d425b04261
심재우 <jw_shim@naver.com>
2021-09-24 23:55:23 +0900

    210924 : BOJ 1330

---
+++ Baekjoon/1330/Main_심재우.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.util.*;
+
+public class Main_BOJ_1330_두수비교하기_B4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
