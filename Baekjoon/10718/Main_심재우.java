4173ac482eacd3783ab56e17ee30547ec320cc03
심재우 <jw_shim@naver.com>
2021-09-25 00:57:20 +0900

    210925 : BOJ 10718 심재우

---
+++ Baekjoon/10718/Main_심재우.java
@@ -0,0 +1,6 @@
+public class Main_BOJ_10718_Welovekriii_B5 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
