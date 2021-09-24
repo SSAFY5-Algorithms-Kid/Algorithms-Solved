49488c85e7e080e76b890e2cd8bb6880e10369a4
심재우 <jw_shim@naver.com>
2021-09-24 23:42:42 +0900

    BOJ 1000 심재우

---
+++ Baekjoon/1000/Main_심재우.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main_심재우 {
+    public static void main(String[] args) {
+        Scanner scann=new Scanner(System.in);
+        int a=scann.nextInt();
+        int b=scann.nextInt();
+        System.out.println((a+b));
+    }
+}
